d174dfa132513e66ea674f49e6ae981df8d1d40b
Kai-Animator <kaireml@protonmail.com>
2023-03-17 14:05:35 +0900

    docker structure

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { serverEndpoints } from "./src/server";
 
 const server = serverEndpoints();
 
-const PORT: number = 4000;
+const PORT: number = 8080;
 
 server.listen(PORT, () => {
   console.info(`⚡️[Niidl Server]: Is listening to PORT: ${PORT}`);
