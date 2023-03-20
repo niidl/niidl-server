156c8b8f6c4bfff03503a953cf14a1a5d1f3c4af
Fabio Hideki Tabata Hirose <hidekihirose@MacBook-Pro-de-Fabio.local>
2023-03-20 17:19:17 +0900

    Change user model and controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,11 +3,27 @@ import { Request, Response } from 'express';
 
 export async function view(req: Request, res: Response) {
   try {
-    const uid = req.body.uid;
+    const { uid, displayName, email } = req.body.uid;
+    const splitName = displayName.split(' ');
+    const firstName = splitName[0];
+    const lastName = splitName[1];
     const user = await userModel.getUser(uid);
+    const userInfoJson = await fetch('https://api.github.com/user/' + uid);
+    const userInfo = await userInfoJson.json();
+
     if (user) {
-      res.status(200).send('Hello');
+      res.status(200).send(userInfo.login);
     } else {
+      const payload = {
+        id: uid,
+        email,
+        first_name: firstName,
+        last_name: lastName,
+        user_name: userInfo.login,
+        github_url: userInfo.html_url,
+      };
+
+      await userModel.create(payload);
       res.send('');
     }
   } catch (error: any) {
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -2,9 +2,14 @@ import { db } from '../utils/db.server';
 
 type User = {
   id: string;
+  first_name: string;
+  last_name: string;
+  github_url: string;
+  email: string;
+  user_name: string;
 };
 
-export async function getUser(uid: string): Promise<User | null> {
+export async function getUser(uid: string): Promise<object | null> {
   return db.user_account.findUnique({
     select: {
       id: true,
@@ -12,5 +17,14 @@ export async function getUser(uid: string): Promise<User | null> {
     where: {
       id: uid,
     },
+  });
+}
+
+export async function create(payload: Omit<User, 'id'>): Promise<object> {
+  return db.user_account.create({
+    select: {
+      id: true,
+    },
+    data: payload,
   });
 }
