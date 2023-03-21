299a39c6261e0c0c35380923fd2f135166765a5f
MrBCendales <bn.cendales10@gmail.com>
2023-03-21 15:10:16 +0900

    feat: Objects created project controller

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,6 +1,6 @@
-import * as projectModel from './project.model';
-import { Request, Response } from 'express';
-import axios from 'axios';
+import * as projectModel from "./project.model";
+import { Request, Response } from "express";
+import axios from "axios";
 
 export async function index(req: Request, res: Response) {
   try {
@@ -18,34 +18,48 @@ export async function view(req: Request, res: Response) {
     ///////////////////
     const gitAccount = specificProject.github_url; //This url is fetched from the ServerSide github URL
     const GitAPI = {
-      root: 'https://api.github.com/repos/',
-      projectName: '',
-      user: '',
+      root: "https://api.github.com/repos/",
+      projectName: "",
+      user: "",
     };
-    const temp = gitAccount.split('/');
+    const temp = gitAccount.split("/");
     GitAPI.projectName = temp[temp.length - 1];
     GitAPI.user = temp[temp.length - 2];
 
     const apiLink =
       GitAPI.root +
       GitAPI.user +
-      '/' +
+      "/" +
       GitAPI.projectName +
-      '/' +
-      'contributors';
+      "/" +
+      "contributors";
     const apiLink_directory =
-      GitAPI.root + GitAPI.user + '/' + GitAPI.projectName + '/' + 'contents';
+      GitAPI.root + GitAPI.user + "/" + GitAPI.projectName + "/" + "contents";
     const apiLink_issues =
-      GitAPI.root + GitAPI.user + '/' + GitAPI.projectName + '/' + 'issues';
+      GitAPI.root + GitAPI.user + "/" + GitAPI.projectName + "/" + "issues";
 
     const response = await axios.get(apiLink);
     const res_directory = await axios.get(apiLink_directory);
-    const res_issues = await axios.get(apiLink_issues);
+    const data_issues = await axios.get(apiLink_issues);
+
+    const res_contributors = {
+      contributor_id: response.data.id,
+      username: response.data.login,
+    };
+
+    const res_issues = {
+      issue_id: data_issues.data.id,
+      html_url: data_issues.data.html_url,
+      title: data_issues.data.title,
+      created_at: data_issues.data.created_at,
+      issue_author: data_issues.data.user.login,
+      author_id: data_issues.data.user.id,
+    };
 
     const allProjectInfo = {
-      issues: res_issues.data,
-      contributors: response.data,
-      directory: res_directory.data,
+      issues: res_issues,
+      contributors: res_contributors,
+      directory: apiLink_issues,
       ...specificProject,
     };
     ///////////////////
