69c15886d5e5aa2e5727a4a7bae69b2a313c2beb
Takuya Stern <andrewtakuya@gmail.com>
2023-03-27 12:11:31 +0900

    feat: additional documentation

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -15,3 +15,17 @@ export async function validateUser(sessionId: string): Promise<UserId | null> {
       },
     });
   }
+
+/* Controller side template
+    try {
+        const cookieObj: { sessionToken: string } = req.cookies;
+        const sessionId: string = cookieObj.sessionToken;
+        const ghuid = await authModel.validateUser(sessionId);
+
+        if (!ghuid) {
+        return res.status(404).send('Invalid Access Token');
+        }
+    }catch (error: any) {
+        res.status(404).send(error.message);
+    }
+*/
