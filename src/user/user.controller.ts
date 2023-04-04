cb619f416bed580c7e597d5aa7b5d8118288899b
Kai-Animator <kaireml@protonmail.com>
2023-04-04 22:03:22 +0900

    errors fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -163,9 +163,8 @@ export async function save(req: Request, res: Response) {
       res.cookie('sessionToken', sessionId, {
         httpOnly: true,
       });
-      res.cookie('user_name', payload.user_name, {
-        httpOnly: true,
-      });
+      res.cookie('userName', payload.user_name, {});
+      res.send(payload);
     }
   } catch (error: any) {
     res.status(500).send(error.message);
